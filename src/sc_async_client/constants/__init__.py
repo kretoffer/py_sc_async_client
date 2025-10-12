6356d7149f600e44855f5870dfdabb2bfb3f2cb3
kretoffer <20kretovartem000@gmail.com>
2025-10-12 15:07:11 +0300

    add constants

---
+++ src/sc_async_client/constants/__init__.py
@@ -0,0 +1,7 @@
+"""
+This source file is part of an OSTIS project. For the latest info, see https://github.com/ostis-ai
+Distributed under the MIT License
+(See an accompanying file LICENSE or a copy at http://opensource.org/licenses/MIT)
+"""
+
+#from sc_async_client.constants.sc_type import ScType
